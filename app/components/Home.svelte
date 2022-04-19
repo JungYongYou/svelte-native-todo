bfe4ddc268d7589135e1d297491395db0087915e
jydeveloper <lukaryu@naver.com>
2022-04-19 15:55:16 +0900

    svelte native todo

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,27 +1,100 @@
-<page>
-    <actionBar title="Home" />
-    <gridLayout>
-        <label class="info">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
-</page>
+<script>
+    import { Template } from 'svelte-native/components'
 
-<script lang="ts">
-    let message: string = "Blank Svelte Native App"
+    let todos = []
+    let dones = []
+    let textFieldValue = ""
+
+    const removeFromList = (list, item) => list.filter(t => t !== item);
+    const addToList = (list, item) => [item, ...list]
+
+    async function onItemTap(args) {
+        let result = await action("What do you want to do with this task?", "Cancel", [
+            "Mark completed",
+            "Delete forever"
+        ])
+
+        console.log(result);
+        let item = todos[args.index]
+        switch (result) {
+            case "Mark completed":
+                dones = addToList(dones, item)
+                todos = removeFromList(todos, item)
+                break;
+            case "Delete forever":
+                todos = removeFromList(todos, item)
+                break;
+            case "Cancel" || undefined:
+                break;
+        }
+    }
+
+    async function onDoneTap(args) {
+        let result = await action("What do you want to do with this task?", "Cancel", [
+            "Mark To Do",
+            "Delete forever"
+        ])
+
+        console.log(result);
+        let item = dones[args.index]
+        switch (result) {
+            case "Mark To Do":
+                todos = addToList(todos, item)
+                dones = removeFromList(dones, item)
+                break;
+            case "Delete forever":
+                dones = removeFromList(dones, item)
+                break;
+            case "Cancel" || undefined:
+                break;
+        }
+    }
+
+    function onButtonTap() {
+        if (textFieldValue === "") return;
+        console.log("New task added: " + textFieldValue + ".");
+        todos = [{ name: textFieldValue }, ...todos]
+        textFieldValue = "";
+    }
 </script>
 
 <style>
-    .info .fas {
-        color: #3A53FF;
+    textField {
+        font-size: 20;
     }
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
+    label {
+        font-size: 18;
+    }
+
+    .todo-item-completed {
+        color: #939393;
+        text-decoration: line-through;
     }
 </style>
+
+<page>
+    <actionBar title="My Tasks" />
+
+    <tabView>
+        <tabViewItem title="To Do">
+            <gridLayout columns="*,120" rows="70,*">
+                <textField col="0" row="0" bind:text="{textFieldValue}" hint="Type new task..." editable="true" on:returnPress="{onButtonTap}" />
+                <button col="1" row="0" text="Add task" on:tap="{onButtonTap}" class="-primary" />
+
+                <listView items="{todos}" on:itemTap="{onItemTap}" row="1" colSpan="2">
+                    <Template let:item>
+                        <label text="{item.name}" textWrap="true" />
+                    </Template>
+                </listView>
+            </gridLayout>
+        </tabViewItem>
+        <tabViewItem title="Completed">
+            <listView items="{dones}" on:itemTap="{onDoneTap}">
+                <Template let:item>
+                    <label text="{item.name}" class="todo-item-completed" textWrap="true" />
+                </Template>
+            </listView>
+        </tabViewItem>
+    </tabView>
+</page>
